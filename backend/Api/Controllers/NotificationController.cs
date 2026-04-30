9d4165b01c1ade2a95f8f2da1f1bf3ff3b575064
HoangMinh255 <minhnguyen09744@gmail.com>
2026-04-30 17:41:00 +0700

    Add Friendship and Notification APIs

--- backend/Api/Controllers/NotificationController.cs
+++ backend/Api/Controllers/NotificationController.cs
@@ -1,105 +1,77 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using InteractHub.Domain.Entities;
-using InteractHub.Application.Interfaces.Services;
-using InteractHub.Application.DTOs.Notification;
+using InteractHub.Persistence.Repositories;
+using InteractHub.Application.Interfaces.Repositories;
 using Microsoft.AspNetCore.Authorization;
-using System.Security.Claims;
-using System.Linq;
 namespace InteractHub.Api.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class NotificationController : ControllerBase
 {
-    public readonly INotificationService _notificationService;
-    public NotificationController(INotificationService notificationRepository)
+    public readonly INotificationRepository _notificationRepository;
+    public NotificationController(INotificationRepository notificationRepository)
     {
-        _notificationService = notificationRepository;
+        _notificationRepository = notificationRepository;
     }
 
-    [HttpGet]
     [Authorize]
+    [HttpGet]
     public async Task<IActionResult> GetAllNotifications()
     {
-        var notifications = await _notificationService.GetAllNotifications();
+        var notifications = await _notificationRepository.GetAll();
         if(notifications == null)
         {
             return new BadRequestResult();
         }
-        return Ok(notifications);
+        return new OkResult();
     }
 
-    [HttpGet("user/{RecipientId}/page/{page}")]
     [Authorize]
-    public async Task<IActionResult> Get10NotificationsByRecipientId(string RecipientId, int page)
+    [HttpGet("user/{RecipientId}")]
+    public async Task<IActionResult> GetNotificationsByUserId(string RecipientId)
     {
-        try{
-            var notifications = await _notificationService.Get10NotificationsByRecipientId(RecipientId,page);
-            if(notifications == null)
-            {
-                return BadRequest(new { message = "Thông báo phải có nội dung." });
-            }
-            return Ok(notifications);
-        }
-        catch(Exception e)
+        var notifications = await _notificationRepository.GetNotificationsByUserId(RecipientId);
+        if(notifications == null)
         {
-            return StatusCode(500, new { message = "Đã xảy ra lỗi hệ thống: ", error = e.Message });
+            return new BadRequestResult();
         }
-        
+        return new OkResult();
     }
 
     [Authorize]
     [HttpGet("{id}")]
     public async Task<IActionResult> GetNotificationById(Guid id)
     {
-        var notification = await _notificationService.GetNotificationById(id);
+        var notification = await _notificationRepository.GetNotificationById(id);
         if(notification == null)
         {
-            return NotFound(new {message = "Không tìm thấy thông báo!"});
+            return new NotFoundResult();
         }
-        return Ok(notification);
+        return new OkResult();
     }
 
     [Authorize]
     [HttpPost]
-    public async Task<IActionResult> CreateNotification([FromBody] CreateNotificationDto notification)
+    public async Task<IActionResult> CreateNotification(Notification notification)
     {
-        // Lấy UserId từ token
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (userId == null) return Unauthorized();
-
-        var createdNotification = await _notificationService.CreateNotification(notification);
-        return Ok(createdNotification);
+        await _notificationRepository.CreateNotification(notification);
+        return new OkResult();
     }
 
     [Authorize]
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateNotification(Guid id)
+    [HttpPut]
+    public async Task<IActionResult> UpdateNotification(Notification notification)
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (userId == null) return Unauthorized();
-        
-
-        var isSucced = await _notificationService.UpdateNotification(id);
-        if(isSucced)
-        {
-            return Ok(new { message = "Cập nhật thông báo thành công!"});
-        }
-        return NotFound(new { message = "Không tìm thấy thông báo hoặc có lỗi khi cập nhật!"});
+        await _notificationRepository.UpdateNotification(notification);
+        return new OkResult();
     }
 
     [Authorize]
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteNotification(Guid id)
+    [HttpDelete]
+    public async Task<IActionResult> DeleteNotification(Notification notification)
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (userId == null) return Unauthorized();
-
-        var isSucced = await _notificationService.UpdateNotification(id);
-        if(isSucced)
-        {
-            return Ok(new { message = "Xóa thông báo thành công!"});
-        }
-        return NotFound(new { message = "Không tìm thấy thông báo hoặc có lỗi khi xóa!"});
+        await _notificationRepository.DeleteNotification(notification);
+        return new OkResult();
     }
 }
--- backend/Application/DTOs/FriendShip/FriendDto.cs
+++
@@ -1,8 +0,0 @@
-namespace InteractHub.Application.DTOs.Friendship;
-public class FriendDto
-{
-    public Guid FriendshipId { get; set; }
-    public string FriendId { get; set; }     // ID của người bạn kia
-    public string FriendName { get; set; }   // Tên của bạn
-    public string FriendAvatar { get; set; } // Ảnh đại diện
-}
