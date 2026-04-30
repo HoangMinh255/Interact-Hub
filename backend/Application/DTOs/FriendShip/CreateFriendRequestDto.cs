9a622cb0a4f5c6c8cc5cf8a3687ef5d3403e366c
HoangMinh255 <minhnguyen09744@gmail.com>
2026-04-30 17:45:04 +0700

    Fix DI bugs and add seed data

--- backend/Application/DTOs/FriendShip/CreateFriendRequestDto.cs
+++
@@ -1,9 +0,0 @@
-namespace InteractHub.Application.DTOs.Friendship;
-
-public class CreateFriendRequestDto
-{
-    public string RequesterId {get; set;}
-    public string ReceiverId {get; set;}
-    public DateTime RequestedAt {get; set;} = DateTime.UtcNow;
-    public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
-}
